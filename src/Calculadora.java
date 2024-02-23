3ed9976cbbdad3c003dfd8378d5e9095b34ecf6f
JorgeSanchez <jorge.ss@tecjerez.edu.mx>
2024-02-23 00:01:56 -0600

    Se aniadieron todos los botones

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -12,7 +12,12 @@ class VentanaCalculadora extends JFrame implements ActionListener{
 	JLabel txtTrRay, txtStand, txtCloock;
 	JTextField boxRes;
 	JButton btnMC, btnMR, btnMM, btnMm, btnMS, btnM,
-			btnPorc, btnRaiz, btnCuad, btnUnoSx;
+			btnPorc, btnRaiz, btnCuad, btnUnoSx,
+			btnCE, btnC, btnDel, btnDiv,
+			btn7, btn8, btn9, btnX,
+			btn4, btn5, btn6, btnRes,
+			btn1, btn2, btn3, btnSum,
+			btnmm, btn0, btnP, btnIgual;
 	
 	public VentanaCalculadora() {
 		
@@ -41,8 +46,8 @@ class VentanaCalculadora extends JFrame implements ActionListener{
 		//Fila 1 ----------------------------------------
 		
 		boxRes = new JTextField("0");
+		gbc.fill = GridBagConstraints.HORIZONTAL;
 		bgcGrid(boxRes, 0, 1, 6, 1);
-		//gbc.fill = GridBagConstraints.HORIZONTAL;
 		
 		//-----------------------------------------------
 		
@@ -83,7 +88,81 @@ class VentanaCalculadora extends JFrame implements ActionListener{
 		bgcGrid(btnUnoSx, 3, 3, 1, 1);
 		
 		//-----------------------------------------------
+		//Fila 4 ----------------------------------------
 		
+		btnCE = new JButton("CE");
+		bgcGrid(btnCE, 0, 4, 1, 1);
+		
+		btnC = new JButton("C");
+		bgcGrid(btnC, 1, 4, 1, 1);
+		
+		btnDel = new JButton("Del");
+		bgcGrid(btnDel, 2, 4, 1, 1);
+		
+		btnDiv = new JButton("÷");
+		bgcGrid(btnDiv, 3, 4, 1, 1);
+		
+		//-----------------------------------------------
+		//Fila 5 ----------------------------------------
+		
+		btn7 = new JButton("7");
+		bgcGrid(btnPorc, 0, 5, 1, 1);
+		
+		btn8 = new JButton("8");
+		bgcGrid(btnRaiz, 1, 5, 1, 1);
+		
+		btn9 = new JButton("9");
+		bgcGrid(btnCuad, 2, 5, 1, 1);
+		
+		btnX = new JButton("X");
+		bgcGrid(btnUnoSx, 3, 5, 1, 1);
+		
+		//-----------------------------------------------
+		//Fila 6 ----------------------------------------
+		
+		btn4 = new JButton("4");
+		bgcGrid(btn4, 0, 6, 1, 1);
+		
+		btn5 = new JButton("5");
+		bgcGrid(btn5, 1, 6, 1, 1);
+		
+		btn6 = new JButton("6");
+		bgcGrid(btn6, 2, 6, 1, 1);
+		
+		btnRes = new JButton("-");
+		bgcGrid(btnRes, 3, 6, 1, 1);
+		
+		//-----------------------------------------------
+		//Fila 7 ----------------------------------------
+		
+		btn1 = new JButton("1");
+		bgcGrid(btn1, 0, 7, 1, 1);
+		
+		btn2 = new JButton("2");
+		bgcGrid(btn2, 1, 7, 1, 1);
+		
+		btn3 = new JButton("3");
+		bgcGrid(btn3, 2, 7, 1, 1);
+		
+		btnSum = new JButton("+");
+		bgcGrid(btnSum, 3, 7, 1, 1);
+		
+		//-----------------------------------------------
+		//Fila 8 ----------------------------------------
+		
+		btnmm = new JButton("±");
+		bgcGrid(btnPorc, 0, 8, 1, 1);
+		
+		btn0 = new JButton("0");
+		bgcGrid(btnRaiz, 1, 8, 1, 1);
+		
+		btnP = new JButton(".");
+		bgcGrid(btnCuad, 2, 8, 1, 1);
+		
+		btnIgual = new JButton("=");
+		bgcGrid(btnUnoSx, 3, 8, 1, 1);
+		
+		//-----------------------------------------------
 		
 		//--------------------------------------------------------------------------------------
 		
